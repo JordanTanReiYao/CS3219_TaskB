d94b18ae825c432172ccd1857d7fc29c734ecc17
JordanTanReiYao <44694137+JordanTanReiYao@users.noreply.github.com>
2020-09-24 03:29:23 +0800

    Correction

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ else
 var port = process.env.PORT || 8080;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express HAHA'));
+app.get('/', (req, res) => res.send('Hello World with Express'));
 
 // Use Api routes in the App
-app.use('/apip', apiRoutes);
+app.use('/api', apiRoutes);
 //app.use(apiRoutes);
 // Launch app to listen to specified port
 
